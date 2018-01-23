76234ea8f746d6734363430a27f02a64dd9b020b
Unknown <diegopm07@gmail.com>
2018-01-23 16:38:53 -0200

    added a lot of files

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,40 +1,37 @@
 "use strict"
 $(document).ready(function () {
-  var move_nav = function () {
-    if ($(window).scrollTop() > 100) {
-      $(".bg-navbar").fadeOut("slow");
-
-
-
-    } else {
-      $(".bg-navbar").fadeIn("slow");
-    }
-  };
-
-
   $('.first-menu li').fadeOut(0);
-  
+  $('.navbar').fadeOut(0);
+
 
   setTimeout(() => {
     $('.second-title').fadeOut(275);
     $('.first-menu li').fadeIn(2500);
   }, 2500);
 
- 
-    $(window).one('scroll',function () {
-      
-      
-        
-        $('.first-page').slideUp(700);
-        $('html, body').animate({scrollTop: '0px'}, 300);
-     
-   
 
-    });
-   
-    
-  
-  
+  $(window).one('scroll', function () {
+    $('.first-page').slideUp(700);
+    $('html, body').animate({ scrollTop: '0px' }, 300);
+    $('.navbar').fadeIn(800);
+  });
+
+  $(window).on('scroll', function () {
+    var navlink_off = $('.nav-link').offset();
+    var pos_slide_off = $('.first-menu').offset();
+    if(navlink_off.top > 100 && pos_slide_off.top == 0){
+      $(".nav-link").css("color","white");
+     
+      $(".navbar").css("background-color","transparent");
+    }else{
+      $(".nav-link").css("color","#1f3646");
+      $(".navbar").css("background-color","#efd09a");
+      
+    }
+  });
+
+
+
 
 
 
